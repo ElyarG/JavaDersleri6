5c0f47eec446a4b9534473af54d62470220b9a62
ElyarG <77214271+ElyarG@users.noreply.github.com>
2021-01-10 04:50:45 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Main  {
+
+    public static void main(String[] args) {
+
+        for (int i = 7; i >0; i--)
+            System.out.println("Hello World " + i);
+
+    }
+}
